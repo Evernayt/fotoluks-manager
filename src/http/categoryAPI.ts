9d3ef54089ea12fec4aa8c640e9902ce2748359d
Evernayt <evernayte@gmail.com>
2022-08-16 13:51:09 +0500

    Create category api

--- src/http/categoryAPI.ts
+++ src/http/categoryAPI.ts
@@ -5,3 +5,8 @@ export const fetchCategoriesAPI = async (): Promise<ICategory[]> => {
   const { data } = await $host.get('api/category/all');
   return data;
 };
+
+export const createCategoryAPI = async (name: string): Promise<ICategory> => {
+  const { data } = await $host.post('api/category/create', { name });
+  return data;
+};
