2fc1141bb22b01ca21fda83fb668da86b021d915
Evernayt <evernayte@gmail.com>
2022-09-13 12:20:31 +0500

    Removed paths

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,7 +1,5 @@
 const LOGIN_ROUTE = '/';
 const ORDERS_ROUTE = '/orders';
-const TASKS_ROUTE = '/tasks';
-const MESSENGER_ROUTE = '/messenger';
 const CONTROL_PANEL_ROUTE = '/control_panel';
 const PROFILE_ROUTE = '/profile';
 const ORDER_DETAIL_ROUTE = '/order_detail';
@@ -11,8 +9,6 @@ const SETTINGS_ROUTE = '/settings';
 export {
   LOGIN_ROUTE,
   ORDERS_ROUTE,
-  TASKS_ROUTE,
-  MESSENGER_ROUTE,
   CONTROL_PANEL_ROUTE,
   PROFILE_ROUTE,
   ORDER_DETAIL_ROUTE,
