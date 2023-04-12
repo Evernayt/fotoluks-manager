2bbdc72466407c5e935b23c5754d04d5bc21e129
Evernayt <evernayte@gmail.com>
2023-04-12 14:07:29 +0500

    Added className

--- src/components/UI/Checkbox/Checkbox.tsx
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -3,11 +3,16 @@ import styles from './Checkbox.module.scss';
 
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   text: string;
+  containerClassName: string;
 }
 
-const Checkbox: FC<CheckboxProps> = ({ text, ...props }) => {
+const Checkbox: FC<CheckboxProps> = ({
+  text,
+  containerClassName,
+  ...props
+}) => {
   return (
-    <div className={styles.container}>
+    <div className={[styles.container, containerClassName].join(' ')}>
       <input
         className={styles.checkbox}
         type="checkbox"
